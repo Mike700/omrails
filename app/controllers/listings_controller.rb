f0771b2a97d10728bde30a6b9162dc95223df7e6
Mike Davison <mike@movesomething.co.uk>
2013-07-21 13:51:58 +0200

    Added Pagination

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,7 +4,7 @@ class ListingsController < ApplicationController
   # GET /listings
   # GET /listings.json
   def index
-    @listings = Listing.all
+    @listings = Listing.order("created_at desc").page(params[:page]).per_page(10)
 
     respond_to do |format|
       format.html # index.html.erb
